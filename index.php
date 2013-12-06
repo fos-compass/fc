6553e1965e415e25ed63dde5357e65c35affef01
Sol Boucher <slb1566@rit.edu>
2013-12-05 20:41:16 -0500

    Give the table a Web 1.0 border

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "<form action=\"result.php\" action=\"get\">"; // TODO validate the results
 echo "<table>";
 foreach($survey as &$question)
 	echo $question;
-echo "<tr><td colspan=\"2\"><input type=\"submit\"/></td></tr>";
+echo "<tr><td class=\"centered\" colspan=\"2\"><input type=\"submit\"/></td></tr>";
 echo "</table>";
 
 tail();
