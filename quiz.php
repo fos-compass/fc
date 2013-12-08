f4a4d368f629dbac5de0b868bb4066d4a0afc964
Steven <skk8768@rit.edu>
2013-12-08 12:36:34 -0500

    added title for the "survey" page

--- quiz.php
+++ quiz.php
@@ -1,7 +1,15 @@
 <?php
 require_once "html.php";
 head();
-
+ ?>
+ 
+<h2>Quiz Instructions</h2>
+<p>For each proposition, decide your level of agreement with that proposition.
+No answer is right or wrong. However, you cannot remain neutral. This test is to
+help guage your particular ideology surrounding free and open source
+culture.</p>
+ 
+<?php
 $tobedisplayed = $survey;
 if(array_key_exists("author", $_GET)) {
 	require_once "author.php";
