ba884a40efad48a39009afd2c8d91d3c03f54c08
Steven <skk8768@rit.edu>
2013-12-08 14:22:56 -0500

    re-indexed the anchor tag id's to match their actual references

--- readlist.php
+++ readlist.php
@@ -4,52 +4,52 @@
 
 <h3>Lawrence Lessig</h3>
 <ul class="cite">
-	<li>[1.0]&nbsp;<a id="0.0" href="http://www.free-culture.cc/get-it/">Free Culture</a></li>
-	<li>[1.0]&nbsp;<a id="0.1" href="http://www.technulogyreview.com/featuredstory/404203/the-people-own-ideas/">The People Own Ideas!</a></li>
+	<li>[1.0]&nbsp;<a id="1.0" href="http://www.free-culture.cc/get-it/">Free Culture</a></li>
+	<li>[1.0]&nbsp;<a id="1.1" href="http://www.technulogyreview.com/featuredstory/404203/the-people-own-ideas/">The People Own Ideas!</a></li>
 </ul>
 
 <h3>Eric Raymond</h3>
 <ul class="cite">
-	<li>[2.0]&nbsp;<a id="1.0" href="http://www.catb.org/esr/">Eric S. Raymond’s Home Page</a></li>
-	<li>[2.1]&nbsp;<a id="1.1" href="http://www.catb.org/~esr/writings/homesteading/cathedral-bazaar/">The Cathedral and the Bazaar</a></li>
-	<li>[2.2]&nbsp;<a id="1.2" href="http://www.linuxtoday.com/infrastructure/2001051501420SCCYMS">Reliance on closed source for security considered harmful</a></li>
-	<li>[2.3]&nbsp;<a id="1.3" href="http://lwn.net/2000/features/ESR/">Eric Raymond Interview </a></li>
-	<li>[2.4]&nbsp;<a id="1.4" href="http://esr.ibiblio.org/?p=556">Why I Hate Proprietary Software</a></li>
+	<li>[2.0]&nbsp;<a id="2.0" href="http://www.catb.org/esr/">Eric S. Raymond’s Home Page</a></li>
+	<li>[2.1]&nbsp;<a id="2.1" href="http://www.catb.org/~esr/writings/homesteading/cathedral-bazaar/">The Cathedral and the Bazaar</a></li>
+	<li>[2.2]&nbsp;<a id="2.2" href="http://www.linuxtoday.com/infrastructure/2001051501420SCCYMS">Reliance on closed source for security considered harmful</a></li>
+	<li>[2.3]&nbsp;<a id="2.3" href="http://lwn.net/2000/features/ESR/">Eric Raymond Interview </a></li>
+	<li>[2.4]&nbsp;<a id="2.4" href="http://esr.ibiblio.org/?p=556">Why I Hate Proprietary Software</a></li>
 </ul>
 
 <h3>Mark Shuttleworth</h3>
 <ul class="cite">
-	<li>[3.0]&nbsp;<a id="2.0" href="http://www.markshuttleworth.com/archives/96">Note to content owners: DRM doesn’t work</a></li>
-	<li>[3.1]&nbsp;<a id="2.1" href="http://www.markshuttleworth.com/archives/115">DRM *really* doesn’t work</a></li>
-	<li>[3.2]&nbsp;<a id="2.2" href="http://developer.ubuntu.com/publish/apps/other-forms-of-submitting-apps/commercial-software-faqs/">Commercial software FAQs</a></li>
-	<li>[3.3]&nbsp;<a id="2.3" href="http://techcrunch.com/2012/05/10/ mark-shuttleworth-is-passionate-about-canonical-patents-and-space/">Mark Shuttleworth is Passionate About Canonical, Patents, and Space</a></li>
-	<li>[3.4]&nbsp;<a id="2.4" href="http://arstechnica.com/gadgets/2013/01/why-mark-shuttleworth-thinks-ubuntu-on-phones-will-outclass-android/">Why Mark Shuttleworth thinks Ubuntu on phones will outclass Android</a></li>
-	<li>[3.5]&nbsp;<a id="2.5" href="http://www.markshuttleworth.com/archives/tag/open-content">Open textbooks to the rescue</a></li>
-	<li>[3.6]&nbsp;<a id="2.6" href="https://archive.org/details/Mark_Shuttleworth_SFD">Mark Shuttleworth Interview for Software Freedom Day (2007)</a></li>
-	<li>[3.7]&nbsp;<a id="2.7" href="http://www.markshuttleworth.com/archives/687">The Responsibility of Ownership</a></li>
-	<li>[3.8]&nbsp;<a id="2.8" href="http://www.bbc.co.uk/news/technulogy-17916879">Ubuntu’s Mark Shuttleworth on shaking up system software</a></li>
+	<li>[3.0]&nbsp;<a id="3.0" href="http://www.markshuttleworth.com/archives/96">Note to content owners: DRM doesn’t work</a></li>
+	<li>[3.1]&nbsp;<a id="3.1" href="http://www.markshuttleworth.com/archives/115">DRM *really* doesn’t work</a></li>
+	<li>[3.2]&nbsp;<a id="3.2" href="http://developer.ubuntu.com/publish/apps/other-forms-of-submitting-apps/commercial-software-faqs/">Commercial software FAQs</a></li>
+	<li>[3.3]&nbsp;<a id="3.3" href="http://techcrunch.com/2012/05/10/ mark-shuttleworth-is-passionate-about-canonical-patents-and-space/">Mark Shuttleworth is Passionate About Canonical, Patents, and Space</a></li>
+	<li>[3.4]&nbsp;<a id="3.4" href="http://arstechnica.com/gadgets/2013/01/why-mark-shuttleworth-thinks-ubuntu-on-phones-will-outclass-android/">Why Mark Shuttleworth thinks Ubuntu on phones will outclass Android</a></li>
+	<li>[3.5]&nbsp;<a id="3.5" href="http://www.markshuttleworth.com/archives/tag/open-content">Open textbooks to the rescue</a></li>
+	<li>[3.6]&nbsp;<a id="3.6" href="https://archive.org/details/Mark_Shuttleworth_SFD">Mark Shuttleworth Interview for Software Freedom Day (2007)</a></li>
+	<li>[3.7]&nbsp;<a id="3.7" href="http://www.markshuttleworth.com/archives/687">The Responsibility of Ownership</a></li>
+	<li>[3.8]&nbsp;<a id="3.8" href="http://www.bbc.co.uk/news/technulogy-17916879">Ubuntu’s Mark Shuttleworth on shaking up system software</a></li>
 </ul>
 
 <h3>Richard Stallman</h3>
 <ul class="cite">
-	<li>[4.0]&nbsp;<a id="3.0" href="http://www.gnu.org/philosophy/opposing-drm.html">Opposing Digital Rights Management</a></li>
-	<li>[4.1]&nbsp;<a id="3.1" href="http://stallman.org/articles/end-war-on-sharing.html">Ending the War on Sharing"</a></li>
-	<li>[4.2]&nbsp;<a id="3.2" href="https://www.gnu.org/distros/common-distros.html">Explaining Why We Don't Endorse Other Systems</a></li>
-	<li>[4.3]&nbsp;<a id="3.3" href="https://www.gnu.org/gnu/manifesto.html">The GNU Manifesto</a></li>
-	<li>[4.4]&nbsp;<a id="3.4" href="http://www.gnu.org/philosophy/copyright-versus-community.html#details">Copyright vs. Community</a></li>
-	<li>[4.5]&nbsp;<a id="3.5" href="http://stallman.org/stallman-computing.html">How I do my computing</a></li>
-	<li>[4.6]&nbsp;<a id="3.6" href="http://www.gnu.org/licenses/license-list.html#OpinionLicenses">Various Licenses for Works Stating a Viewpoint</a></li>
-	<li>[4.7]&nbsp;<a id="3.7" href="http://www.gnu.org/philosophy/fighting-software-patents.html">Fighting Software Patents - Singly and Together</a></li>
+	<li>[4.0]&nbsp;<a id="4.0" href="http://www.gnu.org/philosophy/opposing-drm.html">Opposing Digital Rights Management</a></li>
+	<li>[4.1]&nbsp;<a id="4.1" href="http://stallman.org/articles/end-war-on-sharing.html">Ending the War on Sharing"</a></li>
+	<li>[4.2]&nbsp;<a id="4.2" href="https://www.gnu.org/distros/common-distros.html">Explaining Why We Don't Endorse Other Systems</a></li>
+	<li>[4.3]&nbsp;<a id="4.3" href="https://www.gnu.org/gnu/manifesto.html">The GNU Manifesto</a></li>
+	<li>[4.4]&nbsp;<a id="4.4" href="http://www.gnu.org/philosophy/copyright-versus-community.html#details">Copyright vs. Community</a></li>
+	<li>[4.5]&nbsp;<a id="4.5" href="http://stallman.org/stallman-computing.html">How I do my computing</a></li>
+	<li>[4.6]&nbsp;<a id="4.6" href="http://www.gnu.org/licenses/license-list.html#OpinionLicenses">Various Licenses for Works Stating a Viewpoint</a></li>
+	<li>[4.7]&nbsp;<a id="4.7" href="http://www.gnu.org/philosophy/fighting-software-patents.html">Fighting Software Patents - Singly and Together</a></li>
 </ul>
 
 <h3>Linus Torvalds</h3>
 <ul class="cite">
-	<li>[5.0]&nbsp;<a id="4.0" href="">Just for Fun: The Story of an Accidental Revulutionary"</a></li>
-	<li>[5.1]&nbsp;<a id="4.1" href="http://meta.slashdot.org/story/12/10/11/0030249/linus-torvalds-answers-your-questions">Linus Torvalds Answers Your Questions"</a></li>
-	<li>[5.2]&nbsp;<a id="4.2" href="http://www.linuxtoday.com/developer/2003042401126OSKNLL">DRM is Perfectly OK with Linux"</a></li>
-	<li>[5.3]&nbsp;<a id="4.3" href="http://www.nytimes.com/2003/09/28/magazine/the-way-we-live-now-9-28-03-questions-for-linus-torvalds-the-sharer.html">THE WAY LIVE NOW: 9-28-03: QUESTIONS FOR LINUS TORVALDS; The Sharer"</a></li>
-	<li>[5.4]&nbsp;<a id="4.4" href="http://www.theregister.co.uk/2005/04/14/torvalds_attacks_tridgell/">Torvalds knifes Tridgell"</a></li> 
-	<li>[5.5]&nbsp;<a id="4.5" href="http://lucychili.blogspot.com/2006/07/dmca-comments-by-linus-torvalds.html">homebrew: DMCA - Comments by Linus Torvalds"</a></li>
+	<li>[5.0]&nbsp;<a id="5.0" href="">Just for Fun: The Story of an Accidental Revulutionary"</a></li>
+	<li>[5.1]&nbsp;<a id="5.1" href="http://meta.slashdot.org/story/12/10/11/0030249/linus-torvalds-answers-your-questions">Linus Torvalds Answers Your Questions"</a></li>
+	<li>[5.2]&nbsp;<a id="5.2" href="http://www.linuxtoday.com/developer/2003042401126OSKNLL">DRM is Perfectly OK with Linux"</a></li>
+	<li>[5.3]&nbsp;<a id="5.3" href="http://www.nytimes.com/2003/09/28/magazine/the-way-we-live-now-9-28-03-questions-for-linus-torvalds-the-sharer.html">THE WAY LIVE NOW: 9-28-03: QUESTIONS FOR LINUS TORVALDS; The Sharer"</a></li>
+	<li>[5.4]&nbsp;<a id="5.4" href="http://www.theregister.co.uk/2005/04/14/torvalds_attacks_tridgell/">Torvalds knifes Tridgell"</a></li> 
+	<li>[5.5]&nbsp;<a id="5.5" href="http://lucychili.blogspot.com/2006/07/dmca-comments-by-linus-torvalds.html">homebrew: DMCA - Comments by Linus Torvalds"</a></li>
 </ul>
 
 <h3>Bill Gates</h3>
