8978a1b647ec4bbcb32433d767544f90405c6b55
Sol Boucher <slb1566@rit.edu>
2013-12-08 16:14:13 -0500

    Make quiz page have a better description when displaying author responses

--- quiz.php
+++ quiz.php
@@ -1,18 +1,21 @@
 <?php
 require_once "html.php";
 head();
- ?>
+$thisisaquiz = !array_key_exists("author", $_GET);
+
+if($thisisaquiz)
+	echo "<h2>Quiz Instructions</h2>
+		<p>For each proposition, decide your level of agreement with that proposition.
+		No answer is right or wrong. However, you cannot remain neutral. This test is to
+		help guage your particular ideology surrounding free and open source
+		culture.</p>";
+else
+	echo "<h2>" . $authors[$_GET["author"]]->get_auth_name() . "</h2>
+		<p>This is how we believe this author would have completed to the survey.
+		The rightmost column lists the relevant references, as enumerated on the <a href=\"readlist.php\">Reading List</a> page.</p>";
  
-<h2>Quiz Instructions</h2>
-<p>For each proposition, decide your level of agreement with that proposition.
-No answer is right or wrong. However, you cannot remain neutral. This test is to
-help guage your particular ideology surrounding free and open source
-culture.</p>
- 
-<?php
 $tobedisplayed = $survey;
-if(array_key_exists("author", $_GET)) {
-	require_once "author.php";
+if(!$thisisaquiz) {
 	$tobedisplayed = $authors[$_GET["author"]]->get_all_the_things();
 }
 
@@ -20,7 +23,7 @@ echo "<form action=\"result.php\" action=\"get\">"; // TODO validate the results
 echo "<table width=\"100%\">";
 foreach($tobedisplayed as &$question)
 	echo $question;
-if(!array_key_exists("author", $_GET))
+if($thisisaquiz)
 	echo "<tr><td class=\"centered\" colspan=\"2\"><input type=\"submit\"/></td></tr>";
 echo "</table>";
 
