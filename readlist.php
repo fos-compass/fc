d8494d61c36a8710e748ee99565d797f7ba20ebc
Steven <skk8768@rit.edu>
2013-12-08 16:23:56 -0500

    ensured all links in readlist.php work

--- readlist.php
+++ readlist.php
@@ -10,7 +10,7 @@
 <h3>Lawrence Lessig</h3>
 <ul class="cite">
 	<li>A0&nbsp;<a id="A0" target="_blank" href="http://www.free-culture.cc/get-it/">Free Culture</a></li>
-	<li>A1&nbsp;<a id="A1" target="_blank" href="http://www.technulogyreview.com/featuredstory/404203/the-people-own-ideas/">The People Own Ideas!</a></li>
+	<li>A1&nbsp;<a id="A1" target="_blank" href="http://www.technologyreview.com/featuredstory/404203/the-people-own-ideas/">The People Own Ideas!</a></li>
 </ul>     
           
 <h3>Eric Raymond</h3>
@@ -26,12 +26,12 @@
 	<li>C0&nbsp;<a id="C0" target="_blank" href="http://www.markshuttleworth.com/archives/96">Note to content owners: DRM doesn’t work</a></li>
 	<li>C1&nbsp;<a id="C1" target="_blank" href="http://www.markshuttleworth.com/archives/115">DRM *really* doesn’t work</a></li>
 	<li>C2&nbsp;<a id="C2" target="_blank" href="http://developer.ubuntu.com/publish/apps/other-forms-of-submitting-apps/commercial-software-faqs/">Commercial software FAQs</a></li>
-	<li>C3&nbsp;<a id="C3" target="_blank" href="http://techcrunch.com/2012/05/10/ mark-shuttleworth-is-passionate-about-canonical-patents-and-space/">Mark Shuttleworth is Passionate About Canonical, Patents, and Space</a></li>
+	<li>C3&nbsp;<a id="C3" target="_blank" href="http://techcrunch.com/2012/05/10/mark-shuttleworth-is-passionate-about-canonical-patents-and-space/">Mark Shuttleworth is Passionate About Canonical, Patents, and Space</a></li>
 	<li>C4&nbsp;<a id="C4" target="_blank" href="http://arstechnica.com/gadgets/2013/01/why-mark-shuttleworth-thinks-ubuntu-on-phones-will-outclass-android/">Why Mark Shuttleworth thinks Ubuntu on phones will outclass Android</a></li>
 	<li>C5&nbsp;<a id="C5" target="_blank" href="http://www.markshuttleworth.com/archives/tag/open-content">Open textbooks to the rescue</a></li>
 	<li>C6&nbsp;<a id="C6" target="_blank" href="https://archive.org/details/Mark_Shuttleworth_SFD">Mark Shuttleworth Interview for Software Freedom Day (2007)</a></li>
 	<li>C7&nbsp;<a id="C7" target="_blank" href="http://www.markshuttleworth.com/archives/687">The Responsibility of Ownership</a></li>
-	<li>C8&nbsp;<a id="C8" target="_blank" href="http://www.bbc.co.uk/news/technulogy-17916879">Ubuntu’s Mark Shuttleworth on shaking up system software</a></li>
+	<li>C8&nbsp;<a id="C8" target="_blank" href="http://www.bbc.co.uk/news/technology-17916879">Ubuntu’s Mark Shuttleworth on shaking up system software</a></li>
 </ul>     
           
 <h3>Richard Stallman</h3>
@@ -48,7 +48,7 @@
           
 <h3>Linus Torvalds</h3>
 <ul class="cite">
-	<li>E0&nbsp;<a id="E0" target="_blank" href="">Just for Fun: The Story of an Accidental Revulutionary"</a></li>
+	<li>E0&nbsp;<a id="E0" target="_blank" href="http://books.google.com/books/about/Just_for_Fun.html?id=6zSWd8Ou8BAC">Just for Fun: The Story of an Accidental Revulutionary</a></li>
 	<li>E1&nbsp;<a id="E1" target="_blank" href="http://meta.slashdot.org/story/12/10/11/0030249/linus-torvalds-answers-your-questions">Linus Torvalds Answers Your Questions"</a></li>
 	<li>E2&nbsp;<a id="E2" target="_blank" href="http://www.linuxtoday.com/developer/2003042401126OSKNLL">DRM is Perfectly OK with Linux"</a></li>
 	<li>E3&nbsp;<a id="E3" target="_blank" href="http://www.nytimes.com/2003/09/28/magazine/the-way-we-live-now-9-28-03-questions-for-linus-torvalds-the-sharer.html">THE WAY LIVE NOW: 9-28-03: QUESTIONS FOR LINUS TORVALDS; The Sharer"</a></li>
