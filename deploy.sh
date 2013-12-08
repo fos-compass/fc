946ab8de76b9d34986b1c498a584ffe1bddf06b0
Steven <skk8768@rit.edu>
2013-12-08 00:33:39 -0500

    added nav bar and new landing page

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files="index.php result.php survey.php html.php author.php color.php style.css"
+files="compass.svg index.php result.php survey.php html.php author.php color.php landing.php style.css"
 
 chmod g+w $files
 scp $files fosc:/srv/www
--- html.php
+++ html.php
@@ -1,14 +1,28 @@
 <?php
 function head() {
 	echo "<html>
-			<head>
-				<title>FOSCompass</title>
-				<link rel=\"stylesheet\" href=\"style.css\"/>
-			</head>
-			<body>";
+	<head>
+		<title>FOSS Compass</title>
+		<link rel=\"stylesheet\" href=\"style.css\"/>
+	</head>
+	<body>";
+
+	echo "<div id=\"div-main\">
+
+<div id=\"div-nav\">
+	<ul class=\"nav\">
+		<li><a class=\"nav-a\" href=\"landing.php\">Home</a></li>
+		<li><a class=\"nav-a\" href=\"index.php\">Take the Quiz</a></li>
+		<li><a class=\"nav-a\" href=\"readlist.php\">Reading List</a></li>
+	</ul>
+</div>
+
+<div id=\"div-content\">";
 }
 
 function foot() {
+	echo "</div>";
+	echo "</div>";
 	echo "</body>";
 	echo "</html>";
 }
