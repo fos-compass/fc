7e709456a9eb2105df280e14b0bfa779463d1655
Steven <skk8768@rit.edu>
2013-12-09 16:25:10 -0500

    added privacy clause

--- index.php
+++ index.php
@@ -15,25 +15,27 @@ characters and music through copyright and patents.</p>
 has appeared throughout the Internet. With it has arisen the Creative Commons and free
 culture.</p>
 
-<p>It is difficult to simply capture the cultural ideology of people today.  New
+<p>It is difficult to simply capture the cultural ideology of people today. New
 technologies have introduced new areas for disagreement and cooperation.  For
 example, the average citizen may not be able to distinguish the views of Richard
 Stallman or Linus Torvalds. While they may generally be grouped in a "freer"
 category, their views do differ.</p>
 
-<p>Our work stresses that one's ideological orientation is more than just a binary
-quality of open and closed. There are many pervasive issues in the area of free and
-open source culture, so it is useful to establish a benchmark that breaks down one's views. Our short quiz will present
-you with a number of propositions. You must determine your agreement with each;
-possible values are Strongly Disagree, Disagree, Agree, and Strongly Agree. It
-is important that you make a choice in either direction.  You cannot remain
-neutral.</p>
+<p>Our work stresses that one's ideological orientation is more than just a
+binary quality of open and closed. There are many pervasive issues in the area
+of free and open source culture, so it is useful to establish a benchmark that
+breaks down one's views. Our short quiz will present you with a number of
+propositions. You must determine your agreement with each; possible values are
+Strongly Disagree, Disagree, Agree, and Strongly Agree. It is important that you
+make a choice in either direction.  You cannot remain neutral.</p>
 
 <p>The questions will probe your views in a number of areas with the goal of
 presenting your ideology on two axes. It is best to take the quiz before
 learning the specific methodology behind the quiz questions. <b>At no time is
 your data logged. The data is used to calculate your result which will remain
-private for your use</b>.</p>
+private for your eyes only. However, this site represents an alpha version of
+our software, so privacy flaws may be present. Use this site at your own
+discretion.</b></p>
 
 <p><a href="quiz.php">Are you ready to take the quiz?</a></p>
 
